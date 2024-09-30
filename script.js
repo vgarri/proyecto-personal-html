0686cc3492ab2960455ac9360c6bd208ba494b93
Victor Garritano <victor@getupgraded.com>
2024-09-30 19:45:36 +0200

    validaciones con regex y postsubmit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 //Validacion Formulario landing page
 document.querySelector("form").addEventListener("submit", function (event) {
     event.preventDefault(); //esto evita enviar el formulario si falta por rellenar algun campo.
@@ -6,6 +7,7 @@ const fname = event.target.fname.value;
 const lname = event.target.lname.value;
 const email = event.target.email.value;
 const color = event.target.color.value;
+const pw = event.target.pw.value;
 let msj = "";
 
 if (fname.length < 2 || fname.length > 20) {
@@ -21,13 +23,27 @@ if (lname.length < 2 || lname.length > 20) {
 
 
 
+//validacion con regExp
 
-if(email.match('@' && ".") === 0){
+/*
+Que empiece por letra o número, y tenga al menos un carácter Para el dominio (ej. @hotmail @gmail):
+Que tenga @
+Que tenga sólo letras, y al menos tres caracteres Para el dominio padre (ej. .es .com .org)
+Podrá tener dos (.es, .fr, .it,…) o tres letras (.com, .net, .org,..) o cuatro (.mobi, info,…)
+Debe ir al final, detrás de un punto
+*La expresión regular sería algo como: expresionregular@expresionregular.expresionregular. Ej: elena@gmail.com; pepe03@hotmail.es Crearemos un formulario que llamará a la función checkEmail. Esta función será la que ejecute la expresión regular. Se le pasará como argumento el valor del email recogido.
+Patrón RegExp: expresionregular@expresionregular.expresionregular
+*/
+let regExp = new RegExp ( /^[a-zA-Z0-9._]+@[a-zA-Z0-9.]+.[a-zA-Z]{2,}$/);
+let regExpPw = new RegExp (/^[a-zA-Z0-9!@#$%&]{6,20}$/)
+if(regExp.test(email) == false){
     console.log("Introduce un email válido.");
-}
-if (color.length < 3) {
+    msj += "Introduce un email válido; formato incorrecto.";
+} if (color.length < 3) {
     console.log("Eso no es un color.");
     msj += " Eso no es un color.";
+} if(regExpPw.test(pw)== false) {
+    msj += "Introduce una contraseña válida; formato incorrecto.";
 }
 
 
@@ -38,8 +54,19 @@ if (msj.length != 0) {
       } else {
         alert("Se han enviado correctamente tus respuestas, gracias! :)")
     event.target.submit();
+    location.replace("postsubmit.html")
     }
-    });
+});
+
+// //boton para volver a home
+// document.querySelector("button").addEventListener("submit", function(evento){
+   
+//     console.log("Click");
+//     window.location.replace = href = "./index.html"
+// });
+
+
+
 
 
 
